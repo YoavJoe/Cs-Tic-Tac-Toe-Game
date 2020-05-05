c1c14c1d90f2da6a7242fbaf45ea94034000bbad
YoavJoe <44302737+YoavJoe@users.noreply.github.com>
2020-05-05 18:39:13 +0300

    code of tic tac toe game menu form functions

---
+++ Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tic_tac_toe_Game
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            tic_tac_toe form1 = new tic_tac_toe();
+            form1.Show();
+        }
+
+        private void btnQuite_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
