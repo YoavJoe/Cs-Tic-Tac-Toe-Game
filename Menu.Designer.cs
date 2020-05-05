91d2d94faba222666c47b2b2bccb7b668c547f13
YoavJoe <44302737+YoavJoe@users.noreply.github.com>
2020-05-05 18:39:41 +0300

    code of tic tac toe game menu form designer

---
+++ Menu.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace tic_tac_toe_Game
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQuite = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnQuite
+            // 
+            this.btnQuite.Location = new System.Drawing.Point(39, 136);
+            this.btnQuite.Name = "btnQuite";
+            this.btnQuite.Size = new System.Drawing.Size(197, 48);
+            this.btnQuite.TabIndex = 3;
+            this.btnQuite.Text = "Quite";
+            this.btnQuite.UseVisualStyleBackColor = true;
+            this.btnQuite.Click += new System.EventHandler(this.btnQuite_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(39, 49);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(197, 50);
+            this.btnNewGame.TabIndex = 2;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnQuite);
+            this.Controls.Add(this.btnNewGame);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuite;
+        private System.Windows.Forms.Button btnNewGame;
+
+    }
+}
