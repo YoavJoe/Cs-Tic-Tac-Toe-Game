77e97f6f55351de5dd0735422f17ec8e5af2efc6
YoavJoe <44302737+YoavJoe@users.noreply.github.com>
2020-05-05 18:36:10 +0300

    adding the code of tic tac toe game designer

---
+++ tic_tac_toe.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace tic_tac_toe_Game
+{
+    partial class tic_tac_toe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Board = new System.Windows.Forms.GroupBox();
+            this.b_2_0 = new System.Windows.Forms.Button();
+            this.b_2_1 = new System.Windows.Forms.Button();
+            this.b_2_2 = new System.Windows.Forms.Button();
+            this.b_1_0 = new System.Windows.Forms.Button();
+            this.b_1_1 = new System.Windows.Forms.Button();
+            this.b_1_2 = new System.Windows.Forms.Button();
+            this.b_0_2 = new System.Windows.Forms.Button();
+            this.b_0_1 = new System.Windows.Forms.Button();
+            this.b_0_0 = new System.Windows.Forms.Button();
+            this.label_1 = new System.Windows.Forms.Label();
+            this.player1_wins = new System.Windows.Forms.Label();
+            this.player2_wins = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Turns_Players = new System.Windows.Forms.Label();
+            this.btnQuite = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.Board.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Board
+            // 
+            this.Board.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.Board.BackgroundImage = global::tic_tac_toe_Game.Properties.Resources.board;
+            this.Board.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Board.Controls.Add(this.b_2_0);
+            this.Board.Controls.Add(this.b_2_1);
+            this.Board.Controls.Add(this.b_2_2);
+            this.Board.Controls.Add(this.b_1_0);
+            this.Board.Controls.Add(this.b_1_1);
+            this.Board.Controls.Add(this.b_1_2);
+            this.Board.Controls.Add(this.b_0_2);
+            this.Board.Controls.Add(this.b_0_1);
+            this.Board.Controls.Add(this.b_0_0);
+            this.Board.Location = new System.Drawing.Point(192, 89);
+            this.Board.Name = "Board";
+            this.Board.Size = new System.Drawing.Size(688, 396);
+            this.Board.TabIndex = 0;
+            this.Board.TabStop = false;
+            this.Board.Text = "Board";
+            // 
+            // b_2_0
+            // 
+            this.b_2_0.BackColor = System.Drawing.Color.White;
+            this.b_2_0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.b_2_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.b_2_0.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.b_2_0.Location = new System.Drawing.Point(50, 289);
+            this.b_2_0.Name = "b_2_0";
+            this.b_2_0.Size = new System.Drawing.Size(119, 73);
+            this.b_2_0.TabIndex = 8;
+            this.b_2_0.UseVisualStyleBackColor = false;
+            this.b_2_0.Click += new System.EventHandler(this.b_Click);
+            // 
+            // b_2_1
+            // 
+            this.b_2_1.BackColor = System.Drawing.Color.White;
+            this.b_2_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.b_2_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.b_2_1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.b_2_1.Location = new System.Drawing.Point(253, 289);
+            this.b_2_1.Name = "b_2_1";
+            this.b_2_1.Size = new System.Drawing.Size(119, 73);
+            this.b_2_1.TabIndex = 7;
+            this.b_2_1.UseVisualStyleBackColor = false;
+            this.b_2_1.Click += new System.EventHandler(this.b_Click);
+            // 
+            // b_2_2
+            // 
+            this.b_2_2.BackColor = System.Drawing.Color.White;
+            this.b_2_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.b_2_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.b_2_2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.b_2_2.Location = new System.Drawing.Point(457, 289);
+            this.b_2_2.Name = "b_2_2";
+            this.b_2_2.Size = new System.Drawing.Size(119, 73);
+            this.b_2_2.TabIndex = 6;
+            this.b_2_2.UseVisualStyleBackColor = false;
+            this.b_2_2.Click += new System.EventHandler(this.b_Click);
+            // 
+            // b_1_0
+            // 
+            this.b_1_0.BackColor = System.Drawing.Color.White;
+            this.b_1_0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.b_1_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.b_1_0.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.b_1_0.Location = new System.Drawing.Point(50, 151);
+            this.b_1_0.Name = "b_1_0";
+            this.b_1_0.Size = new System.Drawing.Size(119, 73);
+            this.b_1_0.TabIndex = 5;
+            this.b_1_0.UseVisualStyleBackColor = false;
+            this.b_1_0.Click += new System.EventHandler(this.b_Click);
+            // 
+            // b_1_1
+            // 
+            this.b_1_1.BackColor = System.Drawing.Color.White;
+            this.b_1_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.b_1_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.b_1_1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.b_1_1.Location = new System.Drawing.Point(253, 151);
+            this.b_1_1.Name = "b_1_1";
+            this.b_1_1.Size = new System.Drawing.Size(119, 73);
+            this.b_1_1.TabIndex = 4;
+            this.b_1_1.UseVisualStyleBackColor = false;
+            this.b_1_1.Click += new System.EventHandler(this.b_Click);
+            // 
+            // b_1_2
+            // 
+            this.b_1_2.BackColor = System.Drawing.Color.White;
+            this.b_1_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.b_1_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.b_1_2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.b_1_2.Location = new System.Drawing.Point(457, 151);
+            this.b_1_2.Name = "b_1_2";
+            this.b_1_2.Size = new System.Drawing.Size(119, 73);
+            this.b_1_2.TabIndex = 3;
+            this.b_1_2.UseVisualStyleBackColor = false;
+            this.b_1_2.Click += new System.EventHandler(this.b_Click);
+            // 
+            // b_0_2
+            // 
+            this.b_0_2.BackColor = System.Drawing.Color.White;
+            this.b_0_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.b_0_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.b_0_2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.b_0_2.Location = new System.Drawing.Point(457, 28);
+            this.b_0_2.Name = "b_0_2";
+            this.b_0_2.Size = new System.Drawing.Size(119, 73);
+            this.b_0_2.TabIndex = 2;
+            this.b_0_2.UseVisualStyleBackColor = false;
+            this.b_0_2.Click += new System.EventHandler(this.b_Click);
+            // 
+            // b_0_1
+            // 
+            this.b_0_1.BackColor = System.Drawing.Color.White;
+            this.b_0_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.b_0_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.b_0_1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.b_0_1.Location = new System.Drawing.Point(253, 28);
+            this.b_0_1.Name = "b_0_1";
+            this.b_0_1.Size = new System.Drawing.Size(117, 73);
+            this.b_0_1.TabIndex = 1;
+            this.b_0_1.UseVisualStyleBackColor = false;
+            this.b_0_1.Click += new System.EventHandler(this.b_Click);
+            // 
+            // b_0_0
+            // 
+            this.b_0_0.BackColor = System.Drawing.Color.White;
+            this.b_0_0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.b_0_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.b_0_0.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.b_0_0.Location = new System.Drawing.Point(50, 28);
+            this.b_0_0.Name = "b_0_0";
+            this.b_0_0.Size = new System.Drawing.Size(119, 73);
+            this.b_0_0.TabIndex = 0;
+            this.b_0_0.UseVisualStyleBackColor = false;
+            this.b_0_0.Click += new System.EventHandler(this.b_Click);
+            // 
+            // label_1
+            // 
+            this.label_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label_1.Location = new System.Drawing.Point(19, 50);
+            this.label_1.Name = "label_1";
+            this.label_1.Size = new System.Drawing.Size(49, 23);
+            this.label_1.TabIndex = 1;
+            this.label_1.Text = "P1:";
+            // 
+            // player1_wins
+            // 
+            this.player1_wins.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.player1_wins.Location = new System.Drawing.Point(70, 50);
+            this.player1_wins.Name = "player1_wins";
+            this.player1_wins.Size = new System.Drawing.Size(42, 23);
+            this.player1_wins.TabIndex = 2;
+            this.player1_wins.Text = "0";
+            // 
+            // player2_wins
+            // 
+            this.player2_wins.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.player2_wins.Location = new System.Drawing.Point(1020, 50);
+            this.player2_wins.Name = "player2_wins";
+            this.player2_wins.Size = new System.Drawing.Size(42, 23);
+            this.player2_wins.TabIndex = 4;
+            this.player2_wins.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(969, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "P2:";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(414, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 27);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Turns:";
+            // 
+            // Turns_Players
+            // 
+            this.Turns_Players.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Turns_Players.Location = new System.Drawing.Point(491, 24);
+            this.Turns_Players.Name = "Turns_Players";
+            this.Turns_Players.Size = new System.Drawing.Size(94, 27);
+            this.Turns_Players.TabIndex = 6;
+            // 
+            // btnQuite
+            // 
+            this.btnQuite.Location = new System.Drawing.Point(542, 510);
+            this.btnQuite.Name = "btnQuite";
+            this.btnQuite.Size = new System.Drawing.Size(197, 48);
+            this.btnQuite.TabIndex = 8;
+            this.btnQuite.Text = "Quite";
+            this.btnQuite.UseVisualStyleBackColor = true;
+            this.btnQuite.Click += new System.EventHandler(this.btnQuite_Click);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(309, 508);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(197, 50);
+            this.btnNewGame.TabIndex = 7;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 600);
+            this.Controls.Add(this.btnQuite);
+            this.Controls.Add(this.Turns_Players);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.player2_wins);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.player1_wins);
+            this.Controls.Add(this.label_1);
+            this.Controls.Add(this.Board);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Board.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Board;
+        private System.Windows.Forms.Button b_0_1;
+        private System.Windows.Forms.Button b_0_0;
+        private System.Windows.Forms.Button b_2_0;
+        private System.Windows.Forms.Button b_2_1;
+        private System.Windows.Forms.Button b_2_2;
+        private System.Windows.Forms.Button b_1_0;
+        private System.Windows.Forms.Button b_1_1;
+        private System.Windows.Forms.Button b_1_2;
+        private System.Windows.Forms.Button b_0_2;
+        private System.Windows.Forms.Label label_1;
+        private System.Windows.Forms.Label player1_wins;
+        private System.Windows.Forms.Label player2_wins;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Turns_Players;
+        private System.Windows.Forms.Button btnQuite;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
